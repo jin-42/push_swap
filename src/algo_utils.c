c3b86b73dcbf9b0e3e655c90bbeaa647a786b980
Faruk-Emre Sulvac <fsulvac@z2r8p6.42lyon.fr>
2024-02-13 18:07:29 +0100

    norme

--- src/algo_utils.c
+++ src/algo_utils.c
@@ -6,7 +6,7 @@
 /*   By: fsulvac < fsulvac@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 14:50:02 by fsulvac           #+#    #+#             */
-/*   Updated: 2024/02/09 15:01:44 by fsulvac          ###   ########.fr       */
+/*   Updated: 2024/02/13 18:07:02 by fsulvac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 t_list	*ft_search_max_value_lst(t_list *lst)
 {
-	t_list	*max;
-	int		max_element;
+	int			max_element;
+	t_list		*max;
 
 	max_element = 0;
 	while (lst)
@@ -32,8 +32,8 @@ t_list	*ft_search_max_value_lst(t_list *lst)
 
 int	ft_get_min_value(t_list **lst)
 {
-	t_list	*head;
 	int		min_value;
+	t_list	*head;
 
 	head = *lst;
 	min_value = INT_MAX;
@@ -48,9 +48,9 @@ int	ft_get_min_value(t_list **lst)
 
 int	ft_get_min_pos(t_list **lst)
 {
-	t_list *head;
-	int min_value;
-	int pos;
+	int				min_value;
+	int				pos;
+	t_list			*head;
 
 	head = *lst;
 	min_value = ft_get_min_value(lst);
